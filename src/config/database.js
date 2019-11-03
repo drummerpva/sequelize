6a1afb52fb5a7c6ad95926295ebdd727d5b9e0d7
Douglas Poma <pia_da_lan@yahoo.com.br>
2019-11-03 17:43:53 -0300

    Up

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: "postgres",
+  host: "192.168.0.121",
+  username: "postgres",
+  password: "docker",
+  database: "sqlnode",
+  define: {
+    timestamps: true,
+    underscored: true
+  }
+};
---
+++ src/controllers/AddressController.js
@@ -0,0 +1,26 @@
+const Address = require("../models/Address");
+const User = require("../models/User");
+
+module.exports = {
+  index: async (req, res) => {
+    const { user_id } = req.params;
+    const user = await User.findByPk(user_id, {
+      include: { association: "addresses" }
+    });
+
+    return res.json(user);
+  },
+  async store(req, res) {
+    const { user_id } = req.params;
+    const { zipcode, street, number } = req.body;
+    const user = await User.findByPk(user_id);
+
+    if (!user) {
+      return res.status(400).json({ error: "User not found" });
+    }
+
+    const address = await Address.create({ zipcode, street, number, user_id });
+
+    res.json(address);
+  }
+};
